a7f0cb823d4e96273948d40099dc29ef7d9db991
Lucas <joohan0224@naver.com>
2022-12-08 19:47:00 -0800

    update link to hw9

---
+++ HW9/Business-search/app/src/main/java/com/example/business_search/BusinessDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.business_search;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BusinessDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_detail);
+    }
+}
---
+++ HW9/Business-search/app/src/main/java/com/example/business_search/BusinessItem.java
@@ -0,0 +1,2 @@
+package com.example.business_search;public class BusinessItem {
+}
